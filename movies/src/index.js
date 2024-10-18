40b5095bb094138661728c9dd445c92df110c7fb
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-10-18 14:10:41 +0100

    Change site header.

--- movies/src/index.js
+++ movies/src/index.js
@@ -5,12 +5,16 @@ import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MovieDetailsPage";
 import FavoriteMoviesPage from "./pages/FavoriteMoviesPage";
 import MovieReviewPage from "./pages/MovieReviewPage";
+import SiteHeader from './components/siteHeader'
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+
 const App = () => {
   return (
     <BrowserRouter>
+      <SiteHeader />
       <Routes>
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
-        <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
+        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
